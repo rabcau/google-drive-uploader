5f95d9c838c96690acdb6f0b43c0e54a16cd658b
rabcau <romanromanian@gmail.com>
2023-02-17 13:23:45 +0300

    Implement optional files delete. Implement service count_items() method, apply black

--- client.py
+++ client.py
@@ -4,56 +4,72 @@ from concurrent.futures import ThreadPoolExecutor
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from pydrive.files import ApiRequestError
+from pydrive.files import ApiRequestError, GoogleDriveFile
 
 
 class UploadClient:
-    def __init__(self, source_path: str, dest: str, dest_id: Optional[str] = None):
+    def __init__(
+        self,
+        source_path: str,
+        dest: str,
+        dest_id: Optional[str] = None,
+        remove: Optional[bool] = None,
+    ):
         self.source_path = source_path
         self.dest = dest
         self.dest_id = dest_id
         self.drive = GoogleDrive(self._auth())
+        self.remove = remove
 
-    def _auth(self):
+    def _auth(self) -> GoogleAuth:
         auth = GoogleAuth()
         auth.LocalWebserverAuth()
         return auth
 
-    def _get_dest_id(self):
+    def _get_dest_id(self) -> str:
         dir_pool = []
         drive_content = self.drive.ListFile({"q": "trashed=false"}).GetList()
         for file in drive_content:
-            if file['title'] == self.dest:
-                dir_pool.append(file['id'])
+            if file["title"] == self.dest:
+                dir_pool.append(file["id"])
         if dir_pool:
             dest_id = dir_pool[-1]
         else:
             gdrive_dir = self._make_dir(self.dest)
-            dest_id = gdrive_dir['id']
+            dest_id = gdrive_dir["id"]
         return dest_id
 
-    def _make_dir(self, name: str):
-        gdrive_dir = self.drive.CreateFile({
-            'title': name,
-            "mimeType": "application/vnd.google-apps.folder"
-        })
+    def _make_dir(self, name: str) -> GoogleDriveFile:
+        gdrive_dir = self.drive.CreateFile(
+            {"title": name, "mimeType": "application/vnd.google-apps.folder"}
+        )
         gdrive_dir.Upload()
         return gdrive_dir
 
-    def _upload_file(self, file):
+    def upload_file(self, file):
         file_path = os.path.join(self.source_path, file)
         try:
-            f = self.drive.CreateFile({
-                'title': file,
-                "parents": [{"id": self.dest_id}]
-            })
+            f = self.drive.CreateFile(
+                {"title": file, "parents": [{"id": self.dest_id}]}
+            )
             f.SetContentFile(file_path)
             f.Upload()
         except ApiRequestError as exc:
-            print(f'Failed to upload {file}. Error message: {exc}')
+            print(f"Failed to upload {file}. The reason - {exc}")
+        if self.remove:
+            try:
+                os.remove(file_path)
+            except Exception as exc:
+                print(f"Failed to remove {file}. The reason - {exc}")
 
-    def upload(self):
+    def upload_dir(self):
         self.dest_id = self.dest_id or self._get_dest_id()
         files = os.listdir(self.source_path)
-        with ThreadPoolExecutor() as pool:
-            pool.map(self._upload_file, files)
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.upload_file, files)
+
+    def count_items(self) -> int:
+        drive_content = self.drive.ListFile(
+            {"q": f"'{self.dest_id}' in parents and trashed=false"}
+        ).GetList()
+        return len(drive_content)
--- upload.py
+++ upload.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 import argparse
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 
 from client import UploadClient
 
 
-def get_dir(path: str):
+def get_dir(path: str) -> Optional[str]:
     dirs = [dir_ for dir_ in path.split("/") if dir_]
     if dirs:
         return dirs[-1]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     parser = argparse.ArgumentParser()
 
     parser.add_argument("source", help="Path to source dir")
     parser.add_argument("--dest", help="Google Drive dir name")
     parser.add_argument("--dest_id", help="The id of destination dir on Google Drive")
+    parser.add_argument("-r", action="store_const", const=True)
 
     args = parser.parse_args()
 
@@ -27,5 +29,5 @@ if __name__ == '__main__':
     dest = args.dest or get_dir(source)
     dest_id = os.getenv("DEST_ID", args.dest_id)
 
-    client = UploadClient(source, dest, dest_id)
-    client.upload()
+    client = UploadClient(source, dest, dest_id, args.r)
+    client.upload_dir()
